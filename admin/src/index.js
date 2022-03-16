6998cf47cb5e581adcdf466fb38afdf4cd4049c9
Jianzu Guan <jianzuguan@luminalearning.com>
2022-03-16 23:16:38 +0000

    convert .then to async await

--- admin/src/index.js
+++ admin/src/index.js
@@ -8,44 +8,42 @@ const app = express()
 
 app.use(bodyParser.json({limit: "10mb"}))
 
-app.get("/investments/:id", (req, res) => {
+app.get("/investments/:id", async (req, res) => {
   const {id} = req.params
-  axios
-    .get(`${config.investmentsServiceUrl}/investments/${id}`)
-    .then((response) => {
-      // Assume the response is array with one element
-      const investments = response.data[0]
-      if (
-        !Array.isArray(investments.holdings) ||
-        investments.holdings.length === 0
-      ) {
-        res.send("No holdings found")
+  try {
+    const response = await axios.get(
+      `${config.investmentsServiceUrl}/investments/${id}`
+    )
+
+    // Assume the response is array with one element
+    const investments = response.data[0]
+    if (
+      !Array.isArray(investments.holdings) ||
+      investments.holdings.length === 0
+    ) {
+      res.send("No holdings found")
+    }
+
+    const {userId, firstName, lastName, investmentTotal, date, holdings} =
+      investments
+
+    const holdingDetails = holdings.map((holding) => {
+      const {id: companyId, investmentPercentage} = holding
+      const value = investmentTotal * investmentPercentage
+      return {
+        userId,
+        firstName,
+        lastName,
+        date,
+        companyId,
+        value,
       }
-      return investments
     })
-    .then((investments) => {
-      const {userId, firstName, lastName, investmentTotal, date, holdings} =
-        investments
-      return holdings.map((holding) => {
-        const {id: companyId, investmentPercentage} = holding
-        const value = investmentTotal * investmentPercentage
-        return {
-          userId,
-          firstName,
-          lastName,
-          date,
-          companyId,
-          value,
-        }
-      })
-    })
-    .then((holdings) => {
-      res.json(holdings)
-    })
-    .catch((err) => {
-      console.error(err)
-      res.send(500)
-    })
+    console.log(holdingDetails)
+  } catch (err) {
+    console.error(err)
+    res.send(500)
+  }
 })
 
 app.listen(config.port, (err) => {
