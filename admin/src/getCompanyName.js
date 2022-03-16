ff6293c4228b4488a1dbf0c4a4122b14551187b7
Jianzu Guan <jianzuguan@luminalearning.com>
2022-03-16 23:15:09 +0000

    update get company name to pass tests

--- admin/src/getCompanyName.js
+++ admin/src/getCompanyName.js
@@ -1,16 +1,15 @@
 const axios = require("axios")
 const config = require("config")
 
-module.exports = (id) => {
-  return axios
-    .get(`${config.financialCompaniesServiceUrl}/company/${id}`)
-    .then((response) => {
-      const company = response.data
+module.exports = async (id) => {
+  const response = await axios.get(
+    `${config.financialCompaniesServiceUrl}/companies/${id}`
+  )
+  const company = response.data
 
-      if (!("name" in company)) {
-        throw new Error("Company not found")
-      }
+  if (!company || !("name" in company)) {
+    throw new Error("Company not found")
+  }
 
-      return company.name
-    })
+  return company.name
 }
